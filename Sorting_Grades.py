7a06ab2521f3e696f871a579ae670a68e6f42fe3
Rakshit-Chadgal <subservience0@gmail.com>
2026-04-28 00:37:49 +0530

    It will Sort Number of Students by their Grades from A to D

---
+++ Sorting_Grades.py
@@ -0,0 +1,9 @@
+#In this we have a list of grades acquired by a number of students. We will count the number of students with "A" grade, store the value in a list and sort them from "A" to "D"
+G = ["C", "D", "A", "A", "B", "B", "A"]
+S = []
+S.append(G.count("A"))
+S.append(G.count("B"))
+S.append(G.count("C"))
+S.append(G.count("D"))
+S.sort(reverse=True) #This will sort the list in descending order, so the highest count (for "A") will be first, followed by "B", "C", and "D".
+print(S)
