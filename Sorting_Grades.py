da4e76469f0ab8864f0ec8b24896bb05cd9a4a63
Rakshit-Chadgal <subservience0@gmail.com>
2026-04-28 00:44:01 +0530

    Final Changes

--- Sorting_Grades.py
+++ Sorting_Grades.py
@@ -1,9 +1,12 @@
 #In this we have a list of grades acquired by a number of students. We will count the number of students with "A" grade, store the value in a list and sort them from "A" to "D"
 G = ["C", "D", "A", "A", "B", "B", "A"]
+G.sort()
+print("Grades Sorted from A to D: ", G)
+
 S = []
 S.append(G.count("A"))
 S.append(G.count("B"))
 S.append(G.count("C"))
 S.append(G.count("D"))
 S.sort(reverse=True) #This will sort the list in descending order, so the highest count (for "A") will be first, followed by "B", "C", and "D".
-print(S)
+print("Number of Students sorted by grade from A to D: ",S)
