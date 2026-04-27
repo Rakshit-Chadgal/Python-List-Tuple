edbde54c36b94c1e299e2472ec4acabe119c721a
Rakshit-Chadgal <subservience0@gmail.com>
2026-04-28 00:14:58 +0530

    final Working

---
+++ Storing_elements_in_list.py
@@ -0,0 +1,5 @@
+Movies = []
+Movies.append(input("Enter your First favorite Movie: "))
+Movies.append(input("Enter your Second favorite Movie: "))
+Movies.append(input("Enter your Third favorite Movie: "))
+print(f"Your Favorite Movies are: {Movies}")
